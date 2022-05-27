8b468f50132a1690ec4205d74b84a0209bc75e17
reeneeii <106359541+reeneeii@users.noreply.github.com>
2022-05-27 12:06:53 +0900

    뷰게시판소스 업로드 1차

---
+++ components/Gallery.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-container fluid class="p-4 bg-dark">
+            <b-row>
+                <b-col>  
+	         <b-img thumbnail fluid :src="require('../static/pic1.jpg')" alt="Image 1"></b-img> 
+                </b-col>
+                <b-col>
+		 <b-img thumbnail fluid :src="require('../static/pic2.jpg')" alt="Image 2"></b-img> 
+                </b-col>
+                <b-col>
+                 <b-img thumbnail fluid :src="require('../static/pic3.jpg')" alt="Image 3"></b-img>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark"  variant="success">
+      <b-navbar-brand to="/">Rin's 게시판</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">자유게시판</b-nav-item>
+          <b-nav-item to="/Login">로그인</b-nav-item>
+          <b-nav-item to="/gallery">갤러리</b-nav-item>
+          <b-nav-item to="/boardlist">VUE리스트</b-nav-item>
+          <b-nav-item to="/CalVue">달력보기</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  nama: "Header",
+  data() {
+    return {
+
+    }
+  }
+};
+</script>
+
+
+<style>
+  .greenC {background:}
+</style>
